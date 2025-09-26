0b55274421ba21f6647faa91db42b7215e45f3ea
SaiPranav Madupu <msaipranav2004@gmail.com>
2025-09-26 15:42:33 +0530

    changed few files

--- utils/logger.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-def setup_logger(name="trading_bot", log_file="bot.log"):
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s")
-    return logging.getLogger(name)
